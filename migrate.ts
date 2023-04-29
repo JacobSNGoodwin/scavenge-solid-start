0ba9d972fc26f532d43164406486941c387d25fb
Jacob Goodwin <jacob.goodwin@gmail.com>
2023-04-29 11:21:06 -0700

    use tsx for migration script

--- migrate.ts
+++ migrate.ts
@@ -1,6 +1,6 @@
 import Database from 'better-sqlite3';
-import { drizzle } from 'drizzle-orm/better-sqlite3';
 import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
+import { drizzle } from 'drizzle-orm/better-sqlite3';
 
 const sqlite = new Database('scavenge.db');
 const db = drizzle(sqlite);
